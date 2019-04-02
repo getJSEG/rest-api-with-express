02431451d10da6b1f1f50f4967073bc619560fc4
Elmer Gonzalez <elmergonzalez712@gmail.com>
2019-04-01 19:34:27 -0500

    removed the ability to save user session

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,10 +3,11 @@ const User = require('../models/users').User;
 
 function userAuthentication(req, res, next){
   const credentials = auth(req);
-  
-  if(req.session && req.session.userId){
-    next();
-  }else if(credentials){
+  // this code saves the seesion so you dont have to authenticate over again
+  // if(req.session && req.session.userId){
+  //   next();
+  // }else
+  if(credentials){
     User.authenticate(credentials.name, credentials.pass, function (error, user) {
          if(error || !user){
            const err = new Error('Wrong Email or Password');
